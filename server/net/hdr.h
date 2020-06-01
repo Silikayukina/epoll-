de76b46b36db49e77a8af0b77da07d1eb9e9c62b
yukina <1763951926@qq.com>
2020-06-01 18:20:51 +0800

    first commit

---
+++ server/net/hdr.h
@@ -0,0 +1,35 @@
+#ifndef HDR_H
+#define HDR_H
+
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define LOCALPORT 	5150
+#define REMOTEPORT 	7000
+
+#define MAXEVENTS 100
+#define MAXCONNEC  10
+
+struct bidirectional {
+
+	int sendSock;
+	int recvSock;
+	char ip[MAXBUFLEN];
+    
+};
+
+struct msg
+{
+   unsigned int addr;
+   char buf[1];
+}
+
+#endif
